46993a83d5ff0cc0e95c1a6c5b274dca88eb232e
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2020-09-14 16:10:34 -0300

    Project created

---
+++ backend/src/test/java/io/github/douglasantunes/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.douglasantunes.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
