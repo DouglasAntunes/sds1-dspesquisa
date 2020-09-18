39e52d63d3d6a9ac572f3146a868e8a98346dbf6
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2020-09-18 18:48:12 -0300

    Create page Home.

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <>
       <Header />
-      <h2>test</h2>
+      <Home />
     </>
   );
 }
