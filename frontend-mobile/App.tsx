cad1f4407408fc10122975d633669615bf8b2d3a
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2020-09-20 14:59:44 -0300

    Finished backend-mobile.

--- frontend-mobile/App.tsx
+++ frontend-mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { AppLoading } from 'expo';
 import { useFonts, Play_400Regular, Play_700Bold } from '@expo-google-fonts/play';
 import Routes from './src/routes';
--- frontend-mobile/src/pages/CreateRecord/index.tsx
+++ frontend-mobile/src/pages/CreateRecord/index.tsx
@@ -1,16 +1,151 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from '../../components/Header';
+import PlatformCard from './PlatformCard';
+import { Game, GamePlatform } from './types';
+import RNPickerSelect from 'react-native-picker-select';
+import { FontAwesome5 as Icon } from '@expo/vector-icons';
+import axios from 'axios';
+import { RectButton } from 'react-native-gesture-handler';
+
+const gameListPlaceholder = {
+    label: "Selecione o game",
+    value: null
+};
+
+const BASE_URL = "http://blackburn-pc.home.local:8080";
+
+const mapSelectValues = (games: Game[]) => {
+    return games.map(game => ({
+        ...game,
+        label: game.title,
+        value: game.id
+    }));
+}
 
 const CreateRecord = () => {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [platform, setPlatform] = useState<GamePlatform>();
+    const [selectedGame, setSelectedGame] = useState("");
+    const [allGames, setAllGames] = useState<Game[]>([]);
+    const [filteredGames, setFilteredGames] = useState<Game[]>([]);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/games`)
+            .then(response => {
+                setAllGames(mapSelectValues(response.data));
+            })
+            .catch(() => Alert.alert("Erro ao obter a lista de games!"));
+    }, []);
+
+    const handleChangePlatform = (selectedPlatform: GamePlatform) => {
+        setPlatform(selectedPlatform);
+        const gamesByPlatform = allGames.filter((game: Game) => game.platform === selectedPlatform);
+        setFilteredGames(gamesByPlatform);
+    };
+
+    const handleSubmit = () => {
+        const payload = { name, age, gameId: selectedGame };
+        axios.post(`${BASE_URL}/records`, payload)
+            .then(() => {
+                Alert.alert("Dados salvos com sucesso!");
+                setName("");
+                setAge("");
+                setSelectedGame("");
+                setPlatform(undefined);
+            })
+            .catch(() => Alert.alert("Erro ao salvar dados!"));
+    };
 
     return (
         <>
             <Header />
-            <Text>Record</Text>
+            <View style={styles.container}>
+                <TextInput style={styles.inputText}
+                    placeholder="Nome"
+                    placeholderTextColor="#9E9E9E"
+                    onChangeText={text => setName(text)}
+                    value={name}
+                />
+                <TextInput style={styles.inputText}
+                    placeholder="Idade"
+                    placeholderTextColor="#9E9E9E"
+                    keyboardType="numeric"
+                    maxLength={3}
+                    onChangeText={text => setAge(text)}
+                    value={age}
+                />
+                <View style={styles.platformContainer}>
+                    <PlatformCard platform="PC"
+                        onChange={handleChangePlatform}
+                        icon="laptop"
+                        activePlatform={platform}
+                    />
+                    <PlatformCard platform="XBOX"
+                        onChange={handleChangePlatform}
+                        icon="xbox"
+                        activePlatform={platform}
+                    />
+                    <PlatformCard platform="PLAYSTATION"
+                        onChange={handleChangePlatform}
+                        icon="playstation"
+                        activePlatform={platform}
+                    />
+                </View>
+                <RNPickerSelect placeholder={gameListPlaceholder}
+                    onValueChange = {value => setSelectedGame(value)}
+                    value={selectedGame}
+                    items={filteredGames}
+                    style={pickerSelectStyles}
+                    Icon={() => (
+                        <Icon name="chevron-down" color="#9E9E9E" size={25} />
+                    )}
+                 />
+                 <View style={styles.footer}>
+                     <RectButton style={styles.button} onPress={handleSubmit}>
+                         <Text style={styles.buttonText}>SALVAR</Text>
+                     </RectButton>
+                 </View>
+            </View>
         </>
     );
 };
+
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        color: '#ED7947',
+        paddingRight: 30,
+        fontFamily: "Play_700Bold",
+        height: 50
+    },
+    inputAndroid: {
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        color: '#ED7947',
+        paddingRight: 30,
+        fontFamily: "Play_700Bold",
+        height: 50
+    },
+    placeholder: {
+        color: '#9E9E9E',
+        fontSize: 16,
+        fontFamily: "Play_700Bold",
+    },
+    iconContainer: {
+        top: 10,
+        right: 12,
+    }
+});
 
 const styles = StyleSheet.create({
     container: {
